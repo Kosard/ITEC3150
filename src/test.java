32e999104cdbe8f3e3a3fb6e05a703dd6f0f8bfd
Kosard <kevinfigueroab@gmail.com>
2021-01-19 16:46:56 -0500

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String name = in.next();
+
+        System.out.println(name + "is a name");
+    }
+}
